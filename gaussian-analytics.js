741957a2e1f390ed79f80a075fd07e53ae73e0b1
luphord <luphord@protonmail.com>
2020-05-09 22:58:54 +0200

    special case for cdf at 0

--- gaussian-analytics.js
+++ gaussian-analytics.js
@@ -26,6 +26,9 @@ const p = 0.2316419,
  * @returns cumulative distribution of standard normal distribution
  */
 export function cdf(x) {
+    if (x === 0) {
+        return 0.5;
+    }
     let intercept = 1,
         slope = -1;
     if (x <= 0) {
