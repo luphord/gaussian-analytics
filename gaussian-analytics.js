19b51810adc4e513dd2e23fb8a9725fae8fc7983
luphord <luphord@protonmail.com>
2020-05-09 15:03:36 +0200

    jsdoc for pdf

--- gaussian-analytics.js
+++ gaussian-analytics.js
@@ -1,3 +1,8 @@
+/**
+ * Probability density function (pdf) for a standard normal distribution.
+ * 
+ * @param {number} x
+ */
 export function pdf(x) {
     return Math.exp(-0.5*x**2) / Math.sqrt(2 * Math.PI)
 }
