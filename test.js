8724cbf4f368f98bc9fce8755e59dc16f4053413
luphord <luphord@protonmail.com>
2020-05-09 16:30:35 +0200

    test as ES6 module

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-var assert = require('assert');
+import assert from 'assert';
+import * as gauss from './gaussian-analytics.js';
 
 describe('pdf()', function() {
     it('should be symmetric', function() {
-        assert.strictEqual(1.23, 1.23);
+        assert.strictEqual(gauss.pdf(1.23), gauss.pdf(1.23));
     });
 });
